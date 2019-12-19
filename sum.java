491f18d91120d4123b1f8ceb474fd19c9b08079e
dhanshrip35 <dhanshri.pawar@cuelogic.com>
2019-12-19 16:35:14 +0530

    added sum.java file

---
+++ sum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class sum
+{
+	static int add(int a, int b)
+	{
+		return (a+b);
+	}
+
+	public static void main(String args[])
+	{
+		int ans=add(10,20);
+		System.out.println(ans);
+		
+	}
+} 
+
